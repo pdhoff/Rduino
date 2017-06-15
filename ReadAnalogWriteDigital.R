1514f520238c160dd20c385b02e5ad74488ca703
Chris <csuh09@gmail.com>
2017-06-15 16:03:45 -0400

    Tested different baud rates, set newline = 0

--- ReadAnalogWriteDigital.R
+++ ReadAnalogWriteDigital.R
@@ -4,7 +4,7 @@
 library(serial)
 # port can be determined by using listPorts()
 # replace "tty" with "cu", failing to do so will cause R to crash
-con <- serialConnection(name="testCon", port="cu.usbmodem1421", mode="9600,n,8,1", buffering="none", newline=1, translation="cr")
+con <- serialConnection(name="testCon", port="cu.usbmodem1421", mode="19200,n,8,1", buffering="none", newline=0, translation="cr")
 # opens serialConnection for communication, 
 # pause allows communication to be established
 open(con)
