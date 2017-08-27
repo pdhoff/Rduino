828a2dc57143845d1a8d95dc6cc8d94eda40eddb
Chris <csuh09@gmail.com>
2017-08-27 13:32:10 -0400

    Edited dataTracking.R to terminate quicker

--- Examples/dataTracking.R
+++ Examples/dataTracking.R
@@ -2,16 +2,21 @@ source("../BoardControl/functions.R")
 rduino_connect()
 Sys.sleep(2)
 
-Time <- 0:24
-Data <- rep(NA, 25)
+time <- 0:24
+data <- rep(NA, 25)
 off <- get_dpin(4)
+iteration <- 0
+plot(data ~ time, main="Arduino Data", ylim=c(0,1023))
+
 while (!off) {
-	plot(Data ~ Time, main="Arduino Data", ylim=c(0,1023))
-	for (i in 0:24) {
-		obs <- get_apin(5)
-		points(i, obs)
-		Sys.sleep(0.05)
+	if (iteration == length(time)) {
+		iteration <- 0
+		plot(data ~ time, main="Arduino Data", ylim=c(0,1023))
 	}
+	obs <- get_apin(5)
+	points(iteration, obs)
+	Sys.sleep(0.05)
 	off <- get_dpin(4)
+	iteration <- iteration + 1
 }
 rduino_close()
