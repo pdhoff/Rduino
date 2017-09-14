e7aa1b7a99cf2a77b77fda0810d169f9ce1653c0
Chris <csuh09@gmail.com>
2017-09-14 11:11:52 -0400

    Updated for new serial package

--- Examples/flicker.R
+++ Examples/flicker.R
@@ -5,9 +5,9 @@ rduino_connect("19200,n,8,1")
 Sys.sleep(2)  # Allow time for the connection to initialize
 
 for (i in 0:9) {
-	set_dpin(2, 1)
+	set_dpin(8, 1)
 	Sys.sleep(delay)
-	set_dpin(2,0)
+	set_dpin(8,0)
 	Sys.sleep(delay)
 }
 
